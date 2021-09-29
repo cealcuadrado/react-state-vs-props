53416214f1890cc439c5b49a700efe1d9dd7f4ec
CeAlCuadrado <cealcuadrado@outlook.com>
2021-09-29 19:02:02 -0300

    Instaladas dependencias, definido layout

---
+++ src/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <div className="container my-2">
+                <p>Hola, mundo!</p>
+            </div>
+        )
+    }
+}
+
+export default Layout;
